4c6bb79ab3feee21f5688473bdfbb2e490152d78
Art San <artsan.mcc@gmail.com>
2024-07-26 23:31:08 +0700

    add pagination

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -3,7 +3,7 @@
 
 // 12:20
 import './App.css'
-// import Projects from "./components/Projects";
+import Projects from './components/Projects'
 import Todos from './components/Todos'
 
 import Products from './components/Products'
@@ -11,8 +11,8 @@ import Products from './components/Products'
 function App() {
   return (
     <>
-      <Todos />
-      {/* <Projects /> */}
+      {/* <Todos /> */}
+      <Projects />
       {/* <Products /> */}
     </>
   )
