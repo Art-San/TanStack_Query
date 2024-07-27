79006a204aa4f8d506c7021d2a5e0e0ca2938583
Art San <artsan.mcc@gmail.com>
2024-07-27 23:10:40 +0700

    ok

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -2,6 +2,7 @@
 // https://www.youtube.com/watch?v=3e-higRXoaM&t=924s
 
 // 12:20
+// 1:03:00 бесконечная прокрутка
 import './App.css'
 import Projects from './components/Projects'
 import Todos from './components/Todos'
@@ -12,10 +13,45 @@ function App() {
   return (
     <>
       {/* <Todos /> */}
-      <Projects />
-      {/* <Products /> */}
+      {/* <Projects /> */}
+      <Products />
     </>
   )
 }
 
 export default App
+
+// {
+//   const button = document.querySelector('.user-tap-button')
+//   console.log(1, 'button', button)
+//   let reachedZeroEnergy = false
+//   function tick() {
+//     try {
+//       const energy = document.querySelector('.user-tap-energy > p')
+//       console.log(2, 'energy', energy)
+//       if (energy) {
+//         const energyStr = energy.innerText
+//         const currEnergy = Number(energyStr.split('/')[0])
+//         const maxEnergy = Number(energyStr.split('/')[1])
+
+//         if (!reachedZeroEnergy) {
+//           button.dispatchEvent(new PointerEvent('pointerup'))
+//           button.dispatchEvent(new PointerEvent('pointerup'))
+//           button.dispatchEvent(new PointerEvent('pointerup'))
+//           button.dispatchEvent(new PointerEvent('pointerup'))
+//         }
+//         if (currEnergy <= 10) {
+//           reachedZeroEnergy = true
+//         }
+//         if (currEnergy >= maxEnergy - 10) {
+//           reachedZeroEnergy = false
+//         }
+//       }
+//     } catch (e) {
+//       console.log(111, 'error', e)
+//     }
+
+//     setTimeout(tick, 100 * Math.random() + 50)
+//   }
+//   tick()
+// }
